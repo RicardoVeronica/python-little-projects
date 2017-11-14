d9776794ac63de63121e83a4aabe74825362aad8
setcain <charlie.bronson00@gmail.com>
2017-11-13 21:20:47 -0600

    add structure to-do/main.py

--- projects/to-do/main.py
+++ projects/to-do/main.py
@@ -1,4 +1,12 @@
 ''' To do list '''
 
 if __name__ == '__main__':
-    
+
+    print('=== T O  D O  L I S T ===')
+    while True:
+        print('''
+                1 See list
+                2 Add todo item
+                3 Remove todo item
+                ''')
+
