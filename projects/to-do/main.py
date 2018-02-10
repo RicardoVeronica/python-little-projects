55f9ce76c79e4892bf51fa5e83a2b084546c916b
setcain <charlie.bronson00@gmail.com>
2018-02-09 21:34:22 -0600

    update to-do list
    finished function for add item and see your list

--- projects/to-do/main.py
+++ projects/to-do/main.py
@@ -1,12 +1,70 @@
 ''' To do list '''
 
+# Vars
+var_list = list()
+
+
+# Functions
+def show_list():
+    if len(var_list) == 0:
+        print('There are no items on your list')
+    else:
+        print('This is your list: ')
+        for element in var_list:
+            print(element)
+
+
+def add_item():
+    name = input('What is the name of the new item... ')
+
+    add_content = input('Do you want to put extra info on the item? [y/n]: ')
+    if add_content == 'y':
+        content = input('Write content here... ')
+    else:
+        content = ' '
+    
+    item = {'name_key': name, 'content_key': content}
+
+    var_list.append(item)
+    print('''
+            Item added on your list:
+            Name "{}" 
+            Content "{}" '''.format(name, content))
+
+
+def remove_item():
+    print('remove item')
+
+
+def say_good_bye():
+    print('ok good bye') 
+
+
+def what_the():
+    print('Option invalid, try again')
+
+
 if __name__ == '__main__':
 
     print('=== T O  D O  L I S T ===')
-    while True:
-        print('''
-                1 See list
-                2 Add todo item
-                3 Remove todo item
-                ''')
 
+    while True:
+        user = int(input('''
+                What do you want to do?
+                [1] See your list
+                [2] Add item
+                [3] Remove item
+                [4] Out
+                '''))
+        
+        if user == 1:
+            show_list()
+        elif user == 2:
+            add_item()
+        elif user == 3:
+            remove_item()
+        elif user == 4:
+            say_good_bye()
+            break
+        else:
+            what_the()
