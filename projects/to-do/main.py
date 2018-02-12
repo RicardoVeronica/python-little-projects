a8e65d0d9dcd1d4f2d7626ca071bfef773fb077c
setcain <charlie.bronson00@gmail.com>
2018-02-11 18:40:44 -0600

    update to-do/main.py
    add more options

--- projects/to-do/main.py
+++ projects/to-do/main.py
@@ -9,7 +9,7 @@ def show_list():
     if len(var_list) == 0:
         print('There are no items on your list')
     else:
-        print('This is your list: ')
+        print('---------- This is your list: ----------\n')
         for element in var_list:
             print(element)
 
@@ -20,20 +20,33 @@ def add_item():
     add_content = input('Do you want to put extra info on the item? [y/n]: ')
     if add_content == 'y':
         content = input('Write content here... ')
-    else:
+    elif add_content == 'n':
         content = ' '
-    
+    else:
+        print('Press "y" for yes or "n" for no, try again')
+        return add_item()
+
     item = {'name_key': name, 'content_key': content}
 
     var_list.append(item)
     print('''
             Item added on your list:
             Name "{}" 
-            Content "{}" '''.format(name, content))
+            Content "{}" '''.format(name.capitalize(), content.capitalize()))
 
 
 def remove_item():
-    print('remove item')
+    clean = input('Whats the name of the item... ')
+    del(var_list[clean])
+
+
+def update_item():
+    pass
+
+
+
+def delete_list():
+    pass
 
 
 def say_good_bye():
@@ -44,19 +57,25 @@ def what_the():
     print('Option invalid, try again')
 
 
+# Main method
 if __name__ == '__main__':
 
+    # Init
     print('=== T O  D O  L I S T ===')
 
+    # Menu
     while True:
         user = int(input('''
                 What do you want to do?
                 [1] See your list
                 [2] Add item
                 [3] Remove item
-                [4] Out
+                [4] Update item
+                [5] Delete all list
+                [6] Out
                 '''))
         
+        # Call functions
         if user == 1:
             show_list()
         elif user == 2:
@@ -64,6 +83,10 @@ if __name__ == '__main__':
         elif user == 3:
             remove_item()
         elif user == 4:
+            update_item()
+        elif user == 5:
+            delete_list()
+        elif user == 6:
             say_good_bye()
             break
         else:
