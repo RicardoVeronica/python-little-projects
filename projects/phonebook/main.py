b4ec4c794e358b326fbc4f79d1eb59dc56c602e1
setcain <charlie.bronson00@gmail.com>
2018-02-11 18:49:25 -0600

    Add phonebook project
    Create project and add a menu for options

---
+++ projects/phonebook/main.py
@@ -0,0 +1,19 @@
+''' Phonebook '''
+
+# Main method
+if __name__ == '__main__':
+
+    # Init
+    print('=== Welcome to the Phonebook ===')
+
+    # Menu
+    while True:
+        user = str(input('''
+            What do you want to do?..
+            [a]dd a contact
+            [r]emove a contact
+            [s]ee a contact
+            [l]ist all contact
+            '''))
+
+
